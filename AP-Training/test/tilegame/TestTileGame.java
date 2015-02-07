da8d43d782719fa8f8c38904a38c3f46424aaefb
firestar115 <aprime2003@gmail.com>
2015-02-06 17:59:04 -0800

    Add to test case

--- AP-Training/test/tilegame/TestTileGame.java
+++ AP-Training/test/tilegame/TestTileGame.java
@@ -26,5 +26,20 @@ public class TestTileGame extends TestCase
 		toInsert = new NumberTile(4, 2, 9, 8);
 		assertEquals(-1, game.getIndexForFit(toInsert));
 	}
+	
+	@Test
+	public void testInsertTile(){
+		ArrayList<NumberTile> tiles = new ArrayList<NumberTile>();
+		tiles.add(new NumberTile(4, 3, 7, 4));
+		tiles.add(new NumberTile(6, 4, 3, 3));
+		tiles.add(new NumberTile(1, 2, 3, 4));
+		tiles.add(new NumberTile(3, 2, 5, 2));
+		tiles.add(new NumberTile(5, 9, 2, 2));
+		TileGame game = new TileGame(tiles);
+		NumberTile toInsert = new NumberTile(4, 2, 9, 2);
+		assertEquals(true, game.insertTile(toInsert));
+		toInsert = new NumberTile(4, 2, 9, 8);
+		assertEquals(false, game.insertTile(toInsert));
+	}
 
 }
